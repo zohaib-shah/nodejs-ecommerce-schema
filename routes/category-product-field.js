346ac7b8b171b9efb534423ddacaf7a6e84d3811
zohaib-shah <weblearningblog@gmail.com>
2021-08-28 22:56:15 +0500

    category prod fields

--- routes/category-product-field.js
+++ routes/category-product-field.js
@@ -5,54 +5,41 @@ const auth = require('./../middlewares/auth');
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-const category = require('../models/category');
+const CategoryProductField = require('../models/CategoryProductField');
 const User = models.User;
-const Category = models.Category;
 const CategoryProductField = models.CategoryProductField;
 dotenv.config();
 
 router.get('/',async(req,res,next)=>{
       try {
-        const categories = await Category.findAll();
-  res.status(200).json(categories);
+        const category_product_fields = await CategoryProductField.findAll();
+  res.status(200).json(category_product_fields);
       }  catch(err){
           return res.status(500).json(err);
       }
 });
 
-router.get('/:category_id/fields/',async(req,res,next)=>{
-    const { category_id } = req.params;
-    try {
-        console.log(category_id);
-      const category_product_fields = await CategoryProductField.findAll({ where :{ category_id : category_id } });
-      console.log(category_product_fields);
-res.status(200).json(category_product_fields);
-    }  catch(err){
-        return res.status(500).json(err);
-    }
-});
-
 router.post('/', auth ,async(req,res,next)=>{
     const { name, sort_order , parent_id } = req.body;
-    const category = new Category();
+    const CategoryProductField = new CategoryProductField();
     try {
         
         if(name != null){
-            category.name = name;
+            CategoryProductField.name = name;
         } else {
-            throw "Category Name is required";
+            throw "CategoryProductField Name is required";
         }
         if(sort_order != null){
-            category.sort_order = sort_order;
+            CategoryProductField.sort_order = sort_order;
         } else {
-            throw "Category sort order is required";
+            throw "CategoryProductField sort order is required";
         }
         if(parent_id != null){
-            category.parent_id = parent_id;
+            CategoryProductField.parent_id = parent_id;
         } 
         try {
-            await category.save();
-            res.status(201).json(category);
+            await CategoryProductField.save();
+            res.status(201).json(CategoryProductField);
         } catch(err){
             res.status(500).json({"msg":err});
         }
@@ -64,11 +51,11 @@ router.post('/', auth ,async(req,res,next)=>{
 
 router.get('/:id', async(req, res, next) => {
     try {
-        const category = await Category.findByPk(req.params.id);
-        if(category == null){
+        const CategoryProductField = await CategoryProductField.findByPk(req.params.id);
+        if(CategoryProductField == null){
             res.status(404).json({"msg":"Not found"});
         }
-  res.status(200).json(category);
+  res.status(200).json(CategoryProductField);
       }  catch(err){
           return res.status(500).json(err);
       }
@@ -77,22 +64,22 @@ router.get('/:id', async(req, res, next) => {
   router.put('/:id', auth , async(req, res, next) => {//update is only possible from logged in users
    
     try {
-        const category = await Category.findByPk(req.params.id);
-        if(category == null){
+        const CategoryProductField = await CategoryProductField.findByPk(req.params.id);
+        if(CategoryProductField == null){
             res.status(404).json({"msg":"Not found"});
         }
         const { name, sort_order , parent_id } = req.body;
         if(name != null){
-            category.name = name;
+            CategoryProductField.name = name;
         }
         if(sort_order != null){
-            category.sort_order = sort_order;
+            CategoryProductField.sort_order = sort_order;
         }
         if(parent_id != null){
-            category.parent_id = parent_id;
+            CategoryProductField.parent_id = parent_id;
         }
-        category.save();
-  res.status(200).json(category);
+        CategoryProductField.save();
+  res.status(200).json(CategoryProductField);
       }  catch(err){
           return res.status(500).json(err);
       }
