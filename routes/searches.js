65aae33f2479f93334f42692136021ece0ee132b
zohaib-shah <weblearningblog@gmail.com>
2021-08-29 22:01:15 +0500

    Search Attributes routes added

--- routes/searches.js
+++ routes/searches.js
@@ -8,6 +8,7 @@ const dotenv = require('dotenv');
 const category = require('../models/category');
 const User = models.User;
 const Search = models.Search;
+const SearchAttribute = models.SearchAttribute;
 dotenv.config();
 
 router.get('/',async(req,res,next)=>{
@@ -84,5 +85,119 @@ router.get('/:id', async(req, res, next) => {
           return res.status(500).json(err);
       }
   });
+
+  //   search attribute routes start
+router.get('/:search_id/attributes/',async(req,res,next)=>{
+    const { search_id } = req.params;
+    const search = Search.findByPk(search_id);
+    try {
+        if(search == null){
+            throw "Search Not found";
+        }
+    } catch(err){
+        res.status(404).json({"msg":err});//Product not found
+    }
+    try {
+        const search_attributes = await SearchAttribute.findAll({ where :{ search_id : search_id } });
+        res.status(200).json(search_attributes);
+    }  catch(err){
+        return res.status(500).json(err);
+    }
+});
+
+router.get('/:search_id/attributes/:id', async(req, res, next) => {
+    const { search_id , id } = req.params;
+    const search = Search.findByPk(search_id);
+    try {
+        if(search == null){
+            throw "Search Not found";
+        }
+    } catch(err){
+        res.status(404).json({"msg":err});//Product not found
+    }
+    try {
+        const search_attribute = await SearchAttribute.findByPk(id);
+        if(search_attribute == null){
+            res.status(404).json({"msg":"Attribute Not found"});
+        }
+        res.status(200).json(search_attribute);
+      }  catch(err){
+          return res.status(500).json(err);
+      }
+  });
+
+  router.post('/:search_id/attributes/', auth ,async(req,res,next)=>{
+    const { search_id } = req.params;
+    const { value , operator ,category_field_id } = req.body;
+    const attribute = new SearchAttribute();
+    try {
+        if(value == null){
+            throw "Value of the field required";
+        } else {
+            attribute.value = value;
+        }
+        if(category_field_id == null){
+            throw "Every Search Attribute must belong to it's category field";
+        } else {
+            attribute.category_field_id = category_field_id;
+        }
+        const category_product_field = await CategoryProductField.findByPk(category_field_id);
+        const search = await Search.findByPk(search_id);
+        if(category_product_field == null || search == null){
+            throw "Invalid category field or search id given";
+        }
+        attribute.name = category_product_field.name;//copying attribute name from category field 
+        try {
+            attribute.search_id = search_id;
+            attribute.category_field_id = category_field_id;
+            await attribute.save();
+        res.status(201).json(attribute);
+        } catch(err){
+            res.status(500).json(err);
+        }
+        
+    } catch(err){
+        res.status(400).json(err);
+    }
+    
+});
+
+router.put('/:search_id/attributes/:id', auth ,async(req,res,next)=>{
+    const { search_id, id } = req.params;
+    const { value , category_field_id } = req.body;
+    const attribute = await SearchAttribute.findByPk(id);
+    try {
+        if(value != null){
+            attribute.value = value;
+        }
+        if(operator != null){
+            attribute.operator = operator;
+        }
+        if(category_field_id != null){
+            const category_product_field = await CategoryProductField.findByPk(category_field_id);
+            if(category_product_field == null){
+                throw "Category field doesn't exist";
+            }
+            attribute.name = category_product_field.name;
+            attribute.category_field_id = category_field_id;
+        }
+        
+        const search = await Search.findByPk(search_id);
+        if(search == null){
+            throw "Invalid search id given";
+        }
+        try {
+            await attribute.save();
+        res.status(201).json(attribute);
+        } catch(err){
+            res.status(500).json(err);
+        }
+        
+    } catch(err){
+        res.status(400).json(err);
+    }
+    
+});
+// search attribute routes end
 
 module.exports = router;
